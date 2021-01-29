d5664817600812c1892c6006e72cc722184b0a45
wiratama mukti <wrtmukti@gmail.com>
2021-01-29 10:04:22 +0700

    upload proyek

---
+++ app/Http/Controllers/CheckOngkirController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Province;
+use App\Models\City;
+use Kavist\RajaOngkir\Facades\RajaOngkir;
+
+class CheckOngkirController extends Controller
+{
+    public function index()
+    {
+        $provinces = Province::pluck('name', 'province_id');
+        return view('ongkir', compact('provinces'));
+    }
+
+    public function getCities($id)
+    {
+        $city = City::where('province_id', $id)->pluck('name', 'city_id');
+        return response()->json($city);
+    }
+
+    public function check_ongkir(Request $request)
+    {
+        $cost = RajaOngkir::ongkosKirim([
+            'origin'        => $request->city_origin, // ID kota/kabupaten asal
+            'destination'   => $request->city_destination, // ID kota/kabupaten tujuan
+            'weight'        => $request->weight, // berat barang dalam gram
+            'courier'       => $request->courier // kode kurir pengiriman: ['jne', 'tiki', 'pos'] untuk starter
+        ])->get();
+
+        return response()->json($cost);
+    }
+}
